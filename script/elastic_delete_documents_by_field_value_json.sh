484294e5d91666152e6ef348c0c89251f0e2470e
Martin LEKPA <martin.lekpa@ext.saint-gobain.com>
2018-11-08 14:32:15 +0100

    Add merge

--- script/elastic_delete_documents_by_field_value_json.sh
+++ script/elastic_delete_documents_by_field_value_json.sh
@@ -6,12 +6,15 @@
 #CURL="curl -s --cacert /REP/CA.cer"
 CURL="curl -s -k"
 
+LOG="$(dirname $0)/logs/elastic_delete_documents_by_field_value_json.$(date +%Y.%V)"
 JSON=`cat $(dirname $0)/elastic_delete_documents_by_field_value_json.json`
 
+echo "" > $LOG
+
 if jq -e . >/dev/null 2>&1 <<<"$JSON"; then
-	echo "Parsed JSON successfully and got something other than false/null"
+	echo "Parsed JSON successfully and got something other than false/null" >> $LOG
 else
-	echo -e "\n\tFailed to parse JSON ($(dirname $0)/elastic_delete_documents.json) with jq, or got false/null\n"
+	echo -e "\n\tFailed to parse JSON ($(dirname $0)/elastic_delete_documents.json) with jq, or got false/null\n" >> $LOG
 	exit 1
 fi
 
@@ -22,7 +25,7 @@ INDEXLIST="index1 index2"
 
 
 for i in $INDEXLIST; do
-	echo -e "\n * $i"
+	echo -e "\n * $i" >> $LOG
 	FIELDLIST=`echo $JSON | jq -r ".index.$i[].field"`
 	for j in $FIELDLIST; do
 		VALUELIST=`echo $JSON | jq -r ".index.$i[] | select(.field == \"$j\") | .value" | jq -r '.[]'`
@@ -32,10 +35,12 @@ for i in $INDEXLIST; do
 		else
 			TARGETWEEK=`date +%Y.%V -d "$TARGETWEEK"`
 		fi
-		echo -e "\n\t - $j (${TARGETWEEK})"
+		echo -e "\n\t - $j (${TARGETWEEK})" >> $LOG
 		for k in $VALUELIST; do
-			echo -e "\n\t\t . $k"
-			$CURL "${ELASTICURL}/$i-${TARGETWEEK}/_delete_by_query" -XPOST -H "Content-Type: application/json" -d "{ \"query\": { \"match\": { \"$j\": \"$k\" } } }"
+			echo -e "\n\n\t\t . $k\n\t\t\t Deleting..." >> $LOG
+			$CURL "${ELASTICURL}/$i-${TARGETWEEK}/_delete_by_query" -XPOST -H "Content-Type: application/json" -d "{ \"query\": { \"match\": { \"$j\": \"$k\" } } }" >> $LOG
 		done
 	done
+	echo -e "\n\n Merging $i-${TARGETWEEK}..." >> $LOG
+	$CURL "${ELASTICURL}/$i-${TARGETWEEK}/_forcemerge?only_expunge_deletes=true" -XPOST >> $LOG
 done
